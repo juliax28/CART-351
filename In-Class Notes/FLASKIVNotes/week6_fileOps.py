9d082088eb05403835bb889eae15cd34b93ecc67
juliax28 <juliax28@outlook.com>
2025-11-03 13:30:02 -0500

    started flask part 4

---
+++ In-Class Notes/FLASKIVNotes/week6_fileOps.py
@@ -0,0 +1,7 @@
+#how to read and write to files!
+#You can read and write to text files - normal characters 
+#Each line of text is terminated with a END OF LINE character
+# WE wikll focus on TEXT files
+
+# Open rainbow.txt in append mode
+rainbowFile = open("files/rainbow.txt", "a")
